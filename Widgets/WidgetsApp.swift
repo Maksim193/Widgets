bb38bf2387aa6168e11d765a900dc1eea6cca621
Максим Косников <Mayden1127@yandex.ru>
2024-12-26 15:50:13 +0300

    Initial Commit

---
+++ Widgets/WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsApp.swift
+//  Widgets
+//
+//  Created by Максим Косников on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
